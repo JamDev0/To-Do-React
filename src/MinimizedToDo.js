d660ef94249f133c7f9475f1d9dc7f0e68e2ecd3
JAMDeveloper <watermanbr18@gmail.com>
2022-04-28 01:37:26 -0300

    Desenvolvimento do Componente MinimizedToDo

--- src/MinimizedToDo.js
+++ src/MinimizedToDo.js
@@ -4,26 +4,99 @@ import './MinimizedToDo.css';
 
 function MinimizedToDo(Props)
 {   
-    const [DeleteElementState, setDeleteElementState] = useState(RenderDeleteElementSecondState)
+    let MinimizedToDoTouchStart = 0;
+    let NewMinimizedToDoTouchStart = 0;
 
-    function RenderDeleteElementFirstState()
-    {
+    const [DeleteElementState, setDeleteElementState] = useState(()=>{
         return(
-            <div className='Delete'> 
+            <div className='Delete' onClick={RenderDeleteElementSecondState}> 
                 <div className='Img'></div>
                 <span>Apagar?</span>
             </div>
         )
+    })
+
+    function RenderDeleteElementFirstState()
+    {
+        setDeleteElementState(()=>{
+            return(
+                <div className='Delete' onClick={RenderDeleteElementSecondState}> 
+                    <div className='Img'></div>
+                    <span>Apagar?</span>
+                </div>
+            )
+        })
     }
 
     function RenderDeleteElementSecondState()
     {
-        return(
-            <div className='Confirm'> 
-                <span className='Yes'>Sim</span>
-                <span className='No'>Não</span>
-            </div>
-        )
+        setDeleteElementState(()=>{
+            return(
+                <div className='Confirm' tabIndex={0} onBlur={RenderDeleteElementFirstState}> 
+                    <div className='Yes' onClick={Props.DeleteToDoFunction}>
+                        Sim
+                        <div className='Img'></div>
+                    </div>
+                    <div className='No' onClick={RenderDeleteElementFirstState}>
+                        Não
+                        <div className='Img'></div>
+                    </div>
+                </div>
+            )
+        });
+    }
+    
+    useEffect(()=>{
+        Element = document.querySelectorAll('#MinimizedToDoWrapper > .DeleteElementWrapper > .Confirm')[0];
+        if(Element)
+        {
+            Element.focus();
+        }
+    }, [DeleteElementState])
+
+    function DragTheMinimizedToDo(Event)
+    {
+        if(document.querySelectorAll('#MinimizedToDoWrapper > .DeleteElementWrapper > .Confirm')[0])
+        {
+            document.querySelectorAll('#MinimizedToDoWrapper > .DeleteElementWrapper > .Confirm')[0].blur();
+        }
+        
+        NewMinimizedToDoTouchStart = Event.targetTouches[0].clientX;
+        if(MinimizedToDoTouchStart === 0)
+        {
+            MinimizedToDoTouchStart = NewMinimizedToDoTouchStart;
+        }
+        let Element = document.querySelector('#MinimizedToDo');
+        let ElementCurrentLeft;
+        let AmounthOfDrag = (MinimizedToDoTouchStart - NewMinimizedToDoTouchStart)*-1;
+
+
+        console.log('New Cord: ', NewMinimizedToDoTouchStart);
+        
+
+        if(Element.style.left == '')
+        {
+            ElementCurrentLeft = 0;
+        }
+        else
+        {
+            ElementCurrentLeft = parseFloat(Element.style.left.replace('px', ''));
+        }
+        if((ElementCurrentLeft + AmounthOfDrag) > (Element.clientWidth*0.35))
+        {
+            Element.style.left = (Element.clientWidth*0.35) + 'px';
+            MinimizedToDoTouchStart = NewMinimizedToDoTouchStart;
+            return
+        }
+        if((ElementCurrentLeft + AmounthOfDrag) < 0)
+        {
+            Element.style.left = '0px';
+            MinimizedToDoTouchStart = NewMinimizedToDoTouchStart;
+            return
+        }
+        Element.style.left = (ElementCurrentLeft + AmounthOfDrag) + 'px'; 
+        
+        MinimizedToDoTouchStart = NewMinimizedToDoTouchStart;
     }
 
     useEffect(()=>{
@@ -34,8 +107,10 @@ function MinimizedToDo(Props)
         ClampDesc.watch();
     }, [])
     return(
-        <div id='MinimizedToDoWrapper'>
-            {DeleteElementState}
+        <div id='MinimizedToDoWrapper' onTouchMove={DragTheMinimizedToDo} onTouchEnd={()=>{MinimizedToDoTouchStart = 0}}>
+            <div className='DeleteElementWrapper'>
+                {DeleteElementState}
+            </div>
             <div id='MinimizedToDo'>
                 <div className='Content'>
                     <h2>Title</h2>
